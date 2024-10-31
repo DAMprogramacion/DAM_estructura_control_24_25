6024ad042426358315359fd2ce2df338a4732111
ManuelMolino <mmolmil158@g.educaand.es>
2024-10-31 14:41:22 +0100

    ejercicio 7

--- ejercicios/Ejercicio4.java
+++ ejercicios/Ejercicio4.java
@@ -3,17 +3,43 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
 	
+	static Scanner sc = new Scanner(System.in);
+	
+	private static int obtenerNumero() {
+		int numero = 0;
+		do {
+			System.out.println("Introduce número (comprendido entre 1-10)");
+			numero = sc.nextInt();
+		} while (numero < 1 || numero > 10);
+		System.out.printf("%d valor correcto%n", numero);
+		return  numero;
+
+	}
+	
 	public static void main (String[] args) {
+	
+		/*int numero1 = 0, numero2 = 0;
+		do {
+			System.out.println("Introduce número (comprendido entre 1-10)");
+			numero1 = sc.nextInt();
+		} while (numero1 < 1 || numero1 > 10);
+		System.out.printf("%d correcto%n", numero1);
+		do {
+			System.out.println("Introduce número (comprendido entre 1-10)");
+			numero2 = sc.nextInt();
+		} while (numero2 < 1 || numero2 > 10);
+		System.out.printf("%d correcto%n", numero2);*/
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce primer número (comprendido entre 1-10)");
-		int numero1 = sc.nextInt();
-		System.out.println("Introduce segundo número (comprendido entre 1-10)");
-		int numero2 = sc.nextInt();
-		if ( numero1 < 1 || numero1 > 10 || numero2 < 1 || numero2 > 10) {
+		int numero1 = obtenerNumero();
+		int numero2 = obtenerNumero();
+
+		
+		//cambiamos las especificaciones del programa, ahora solicitamos el nº tantas veces
+		//que sea necesario hasta que ambos nº estén comprendidos entre 1-10
+		/*if ( numero1 < 1 || numero1 > 10 || numero2 < 1 || numero2 > 10) {
 			System.out.printf("Los valores %d y %d no pueden ser procesados\n", numero1, numero2);
 			return;
-		}
+		}*/
 		
 		String menu = """
 		    
@@ -30,9 +56,50 @@ public class Ejercicio4 {
 		do {
 			System.out.println(menu);
 			opcion = sc.nextInt();
+			switch(opcion) {
+				case 1 -> {
+					int suma = sumarDosNumeros(numero1, numero2);
+					System.out.printf("%d + %d = %d\n", numero1, numero2, suma);
+				}
+				case 2 -> {
+					int resta = restarDosNumeros(numero1, numero2);
+					System.out.printf("%d - %d = %d\n", numero1, numero2, resta);
+				}
+				case 3 -> {
+					int mult = multiplicarDosNumeros(numero1, numero2);
+					System.out.printf("%d x %d = %d\n", numero1, numero2, mult);
+				}
+				case 4 -> {
+					double div = dividirDosNumeros(numero1, numero2);
+					System.out.printf("%d / %d = %.2f\n", numero1, numero2, div);
+				}
+				case 5 -> {
+					System.out.println("Goodbye!!!");
+					sc.close();  //cerramos el scanner
+				}
+				default -> System.out.println("Opción no permitida");
+			}
 		} while (opcion != 5);
-		
-		
 	}
+	
+	
+	
+	private static int sumarDosNumeros(int a, int b) {
+		return a + b;
+	}
+	private static int restarDosNumeros(int a, int b) {
+		return a - b;
+	}
+	private static int multiplicarDosNumeros(int a, int b) {
+		return a * b;
+	}
+	private static double dividirDosNumeros(int a, int b) {
+		return 1.0 * a / b;
+	}
+	
+	
+	
+	
+	
 }
 
---
+++ ejercicios/ejercicio7/TestEcuaciones.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class TestEcuaciones {
+	
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		//método que nos solicite los seis coeficientes (a, b, c, d, e, f)
+		int a = solicitarCoeficiente();
+		int b = solicitarCoeficiente();
+		int c = solicitarCoeficiente();
+		int d = solicitarCoeficiente();
+		int e = solicitarCoeficiente();
+		int f = solicitarCoeficiente();
+		//vamos a mostrar en consola el sistema de ecuaciones a resolver:
+		mostrarEcuacion(a, b, c, d, e, f);
+		boolean resoluble = esResoluble(a, b, c, d);
+		//System.out.println(resoluble);
+		//si es resoluble decimos que "se resuelve"
+		//en caso contrario indicamos "No es resoluble la ecuación"
+		if (resoluble) {
+			//System.out.println("se resuelve");
+			float x = calcularX(a, b, c, d, e, f);
+			float y = calcularY(a, b, c, d, e, f);
+			System.out.println("Ecuación resoluble, soluciones:");
+			System.out.printf("x = %.2f%n", x);
+			System.out.printf("y = %.2f%n", y);
+		} else 
+			System.out.println("No es resoluble la ecuación");
+	}
+	
+	private static float calcularX(int a, int b, int c, int d, int e, int f) {
+		return 1.0f * (e * d - b * f) / (a * d - b * c);
+	}
+	
+	private static float calcularY(int a, int b, int c, int d, int e, int f) {
+		return 1.0f * (a * f - e * c) / (a * d - b * c);
+	}
+	
+	
+	private static boolean esResoluble(int a, int b, int c, int d) {
+		
+		return a * d - b * c != 0;
+	}
+	
+	
+	private static void mostrarEcuacion(int a, int b, int c, int d, int e, int f) {
+		//VAMOS A RESOLVER EL SIGUIENTE SISTEMAS DE DOS ECUACION Y DOS INCÓGNITAS:
+		// Ecuación 1: 3x  + 5y   = 15
+		// Ecuación 2: -5x  + -5y  = 1
+		//usamos un printf
+		System.out.println("VAMOS A RESOLVER EL SIGUIENTE SISTEMAS DE DOS ECUACION Y DOS INCÓGNITAS:");
+		System.out.printf("Ecuación 1: %dx  + %dy   = %d\n", a, b, e);
+		System.out.printf("Ecuación 2: %dx  + %dy   = %d\n", c, d, f);
+	}
+
+	private static int solicitarCoeficiente() {
+		System.out.println("Dame un valor para un coeficiente de la ecuación");
+		int coeficiente = sc.nextInt();
+		return coeficiente;
+	}
+}
+
